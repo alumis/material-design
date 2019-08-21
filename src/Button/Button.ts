5ddf23c07201d398c67304baf1232221260a44a2
Emil Müller <emil@alumis.no>
2019-08-21 13:34:29 +0200

    bumped

--- src/Button/Button.ts
+++ src/Button/Button.ts
@@ -17,9 +17,13 @@ export class Button extends Component<HTMLButtonElement> {
             delete attrs.theme;
         }
 
-        (this.node = <HTMLButtonElement>createNode("button", attrs, children)).type = submits ? "submit" : "button";
+        let label = createNode("span", null, children);
 
-        this.node.classList.add(Button.styles["mdc-button"]);
+        label.classList.add("mdc-button__label");
+
+        (this.node = <HTMLButtonElement>createNode("button", attrs, label)).type = submits ? "submit" : "button";
+
+        this.node.classList.add("mdc-button");
         this.ripple = new MDCRipple(this.node);
 
         if (isObservable(theme)) {
@@ -53,19 +57,6 @@ export class Button extends Component<HTMLButtonElement> {
             if (!classes.has(c))
                 this.node.classList.remove(c);
     };
-
-    static styles: ButtonStyles;
-}
-
-export interface ButtonStyles {
-
-    "mdc-button": string;
-    "mdc-button--raised": string;
-    "mdc-button--unelevated": string;
-    "mdc-button--outlined": string;
-    "mdc-button--dense": string;
-    "mdc-button__label": string;
-    "mdc-button__icon": string;
 }
 
 export interface ButtonAttributes extends Attributes {
@@ -85,11 +76,11 @@ export enum ButtonTheme {
 function* getThemeClasses(theme: ButtonTheme) {
 
     if (theme & ButtonTheme.Raised)
-        yield Button.styles["mdc-button--raised"];
+        yield "mdc-button--raised";
     if (theme & ButtonTheme.Unelevated)
-        yield Button.styles["mdc-button--unelevated"];
+        yield "mdc-button--unelevated";
     if (theme & ButtonTheme.Outlined)
-        yield Button.styles["mdc-button--outlined"];
+        yield "mdc-button--outlined";
     if (theme & ButtonTheme.Dense)
-        yield Button.styles["mdc-button--dense"];
+        yield "mdc-button--dense";
 }
--- src/_button.scss.d.ts
+++
@@ -1,5 +0,0 @@
-import { ButtonStyles } from "./Button";
-
-declare const styles: ButtonStyles;
-
-export default styles;
