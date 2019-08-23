efa64dd1308a498896070d631907d6c410f34666
Emil Müller <emil@alumis.no>
2019-08-23 14:53:37 +0200

    bumped

--- src/ListItem/ListItem.tsx
+++ src/ListItem/ListItem.tsx
@@ -6,7 +6,6 @@ export class ListItem extends Component<HTMLLIElement> {
     constructor(attrs: ListItemAttributes, children: any[]) {
 
         super();
-
         this.node = <li class="mdc-list-item"><span class="mdc-list-item__text">{children}</span></li>;
     }
 }
